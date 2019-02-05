b61ee3605a464308afb30bc840a9db295e198395
VikasrajYadav <vry420@gmail.com>
2019-02-05 16:46:14 +0530

    storing objects in file name notes-data.json

--- note.js
+++ note.js
@@ -1,26 +1,42 @@
-console.log('Starting Note');
+console.log("Starting Note");
 
+const fs = require("fs");
 
+var addNote = (title, body) => {
+  var notes = [];
+  var note = {
+    title, // actual syntax title: 'title', but if the name is same then we don't have to assign the value
+    body
+  };
 
-var addNote = (title,body) => {
-	 console.log('adding Note',title,body);
-}
+  try {
+    var noteString = fs.readFileSync("notes-data.json");
+    notes = JSON.parse(noteString);
+  } catch (e) {}
+
+  var duplicateNotes = notes.filter((note) => note.title === title);
+
+  if (duplicateNotes.length === 0) {
+    notes.push(note);
+    fs.writeFileSync("notes-data.json", JSON.stringify(notes));
+  }
+};
 
 var getAll = () => {
-	console.log('Getting all notes');
-}
+  console.log("Getting all notes");
+};
 
-var getNote = (title) => {
-	console.log('title');
-}
+var getNote = title => {
+  console.log("title");
+};
 
-var removeNote = (title) => {
-	console.log('removing note',title)
-}
+var removeNote = title => {
+  console.log("removing note", title);
+};
 
 module.exports = {
-	addNote,
-	getAll,
-	getNote,
-	removeNote,
+  addNote,
+  getAll,
+  getNote,
+  removeNote
 };
