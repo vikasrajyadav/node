e22b72fb912dbd62d25cfadb19916e7b2f3c606c
VikasrajYadav <vry420@gmail.com>
2019-02-06 15:12:02 +0530

    comments added to the server.js file

--- web-server/server.js
+++ web-server/server.js
@@ -1,3 +1,7 @@
+//importing package by using require starts here
+
+const fs = require("fs");
+
 const express = require("express");
 const hbs = require("hbs");
 
@@ -7,13 +11,27 @@ hbs.registerPartials(__dirname + "views/partials");
 app.set("view Engine", "hbs");
 app.use(express.static(__dirname + "/public"));
 
+// importing packages ends here
 
-app.use((req,res,next) =>{
-var now = new Date().toString();
-console.log(`${now}: ${req.method} ${req.url}`)
-next();
+// log generate code starts
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+
+  var log = `${now}: ${req.method} ${req.url}`;
+  fs.appendFile("Server.log", log + "\n", err => {
+    if (err) {
+      console.log("Unable to append  server.log");
+    }
+  });
+  console.log();
+  next();
 });
 
+// log generator code ends
+
+
+// webpages with handlebars starts here
 
 app.get("/", (req, res) => {
   res.render("home.hbs", {
@@ -36,6 +54,13 @@ app.get("/bad", (req, res) => {
   });
 });
 
+// webpages with handlebars ends here
+
+
+// port no. assigment starts here
+
 app.listen(3000, () => {
   console.log("Server is up on port no. 3000");
 });
+
+// port no. assigment ends here
