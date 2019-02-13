f01314a5ba875cc158da4628ef1ec12999b0c219
VikasrajYadav <vry420@gmail.com>
2019-02-13 15:49:53 +0530

    UPDATE 13/02/19

--- node-tests/utils/utils.js
+++ node-tests/utils/utils.js
@@ -1,3 +1,10 @@
 module.exports.add = (a,b) => a + b;
 
 module.exports.square = (x) => x*x;
+
+module.exports.setName = (user, fullName) => {
+    var names = fullName.split(' ');
+    user.name = names[0];
+    user.name = names[1];
+    return user;
+};
--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,17 +1,58 @@
+const expect = require("expect");
 const utils = require("./utils");
+
+//test case 1
 
 it("should add two number", () => {
   var res = utils.add(33, 11);
 
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}`);
-  }
+  //mjackson expect testing
+
+  // https://github.com/mjackson/expect check this to more testing option
+
+  expect(res).toBe(44);
+
+  //   if (res !== 44) {
+  //     throw new Error(`Expected 44, but got ${res}`);
+  //   }
 });
+
+//test case 2
 
 it("should square the given number", () => {
   var res = utils.square(3);
 
-  if (res !== 9) {
-    throw new Error(`Expected 9, but got ${res}.`);
-  }
+  //   expect(res).toBe(9).toBeA('number');
+  expect(res).toBe(9);
+
+  //   if (res !== 9) {
+  //     throw new Error(`Expected 9, but got ${res}.`);
+  //   }
 });
+
+//should verify first and last names are set
+//assert it in includes firstname and last name with proper values
+it("Should first name and last name set", () => {
+  var user = { location: "Philadelphia", age: 25 };
+  var res = utils.setName(user, "Andrew Mead");
+
+  // expect(user).toEqual(res);
+
+//   expect(res).toInclude({
+//     firstName: "Andrew",
+//     lastName: "Mead"
+//   });
+});
+
+// it('should expect some values',()=>{
+// // expect(12).toNotBe(12);
+// // expect({name: 'andrew'}).toNotEqual({name:'Andrew'});
+// // expect([2,3,4]).toExclude(1);
+// expect({
+//     name:'Andrew',
+//     age: 25,
+//     location: 'Philadelphia',
+// }).toInclude({
+// age: 25,
+// })
+// });
